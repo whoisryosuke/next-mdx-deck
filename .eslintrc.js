fd35cbec2c1e906b2087b626003db6bd98b127eb
Ryosuke <ryosuke.san.hana@gmail.com>
2020-04-23 12:39:57 -0700

    :wrench: Don't lint MDX with current config

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,12 +2,13 @@ module.exports = {
   parser: 'babel-eslint',
   extends: ['airbnb', 'prettier'],
   plugins: ['react', 'jsx-a11y', 'import'],
+  ignorePatterns: ["slides/"],
   rules: {
     // Next imports React into pages automatically
     'react/react-in-jsx-scope': 'off',
     // Next includes packages like MDX using their bundle
     'import/no-extraneous-dependencies': 'off',
     // Turn off const conversion
-    "prefer-const": 'off'
-  },
+    'prefer-const': 'off',
+  }
 }
