632e017b9e1b12beafef5e970a3c580f073257be
Ryosuke <ryosuke.san.hana@gmail.com>
2020-04-23 15:23:37 -0700

    :sparkles: Syntax highlighting

--- components/MDXProvider.jsx
+++ components/MDXProvider.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import { MDXProvider } from '@mdx-js/react'
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
+import okaidia from 'react-syntax-highlighter/dist/cjs/styles/prism/okaidia'
 import SlidePage from '../layouts/SlidePage'
 import Cover from './Cover'
 
 const mdComponents = {
   h1: (props) => <h1 {...props} />,
+  pre: (props) => props.children,
+  code: (props) => {
+    console.log('syntax props', props)
+    const {className} = props
+    const language = className.replace("language-", "");
+    return (
+      <SyntaxHighlighter className={className} language={language} style={okaidia} {...props} />
+  )},
   Cover,
   SlidePage,
 }
